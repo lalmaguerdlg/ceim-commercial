d1807371c756802fbc149aee7060509c31a755bd
Luis Almaguer <imaximuz@gmail.com>
2020-10-09 20:24:39 -0500

    delete template files

--- template/js/wapp.js
+++
@@ -1,7 +0,0 @@
-var base_url = 'https://wa.me/528110741639?text=';
-$(document).ready(function() {
-    $('a.wapp-redirect').each(function(i, el) {
-        var text = el.getAttribute('wapp-text');
-        el.setAttribute('href', encodeURI(base_url + text));
-    });
-});
