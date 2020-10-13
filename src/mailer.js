a0265b56bd25d5eab0f2f00137f84ffb14f614b7
Luis Almaguer <imaximuz@gmail.com>
2020-10-12 23:28:56 -0500

    add validation to /mail/contact endpoint

--- src/mailer.js
+++ src/mailer.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const nodemailer = require('nodemailer');
-const Mail = require('nodemailer/lib/mailer');
 const env = require('./env');
+
 const router = express.Router();
+
+
 
 async function sendEmail(receiver, subject, html) {
     let transporter = nodemailer.createTransport({
@@ -25,17 +28,28 @@ async function sendEmail(receiver, subject, html) {
     console.log('Email sent: %s', info.messageId);
 }
 
-router.post('/contact', async (req, res) => {
 
-    // const { name, phone, email, subject, message } = req.body;
+router.post('/contact', [
+    body('name').notEmpty({ignore_whitespace: true}).trim().escape(),
+    body('email').trim().isEmail(),
+    body('phone').trim(), //.isMobilePhone('es-MX'), we can maybe validate for only mexican phones
+    body('subject').trim().escape(),
+    body('message').trim().escape(),
+], async (req, res) => {
+
+    const errors = validationResult(req);
+    if( !errors.isEmpty() ){
+        return res.status(400).json({ status: 400, message: "Validation error", errors: errors.array() });
+    }
+
     const body = req.body;
 
     try {
-        await sendEmail('luis.almaguerdlg@gmail.com', body.subject, `${body.name} dijo: ${req.message}`);
-        res.status(200).send({status: "ok"});
+        // await sendEmail('luis.almaguerdlg@gmail.com', body.subject, `${body.name} dijo: ${req.message}`);
+        res.status(200).json({ status: 200, message: "Email sent" });
     } catch(err) {
         console.error(err);
-        res.status(500).send({status: "error"});
+        res.status(500).json({ status: 500, message: "An error occurred when trying to send an email" });
     }
 });
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const body_parser = require('body-parser');
 
 const app = express();
 const mailer = require('./mailer');
@@ -9,14 +8,15 @@ const web = require('./web');
 const PORT = 3000;
 
 
-app.use(body_parser.urlencoded({ extended: true }));
-app.use(body_parser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 app.use('/', web );
 app.use('/mail', mailer );
 
 app.post('*', function(req, res) {
-    res.status(404).send({ error: 404, message: 'Not found'});
+    res.status(404).send({ status: 404, message: 'Not found'});
 });
 
 app.listen(PORT, () => {
