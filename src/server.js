288e3e1a49dc1967218f8473c8561cd5262f0f32
Luis Almaguer <imaximuz@gmail.com>
2020-10-14 13:31:18 -0500

    Add helmet, morgan and error handling middleware

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const argv = require('minimist')(process.argv.slice(2));
 const express = require('express');
+const helmet = require('helmet');
 const { PORT } = require('./env');
 require('./db')(); // This initialices the mongodb connection
 
@@ -8,22 +9,24 @@ const app = express();
 const mailer = require('./mailer');
 const web = require('./web');
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (argv.v || argv.verbose) {
-    app.use((req, res, next) => {
-        const body = Object.keys(req.body).length > 0 ? JSON.stringify(req.body) : '';
-        console.log(`[${new Date().toLocaleString('en-US')}] ${req.method} ${req.path} ${body}`);
-        next();
-    })
+    const morgan = require('morgan');
+    app.use(morgan('tiny'));
 }
 
 const Contact = require('./models/contact');
 
-app.get('/contacts', async (req, res) => {
-    const contacts = await Contact.find();
-    res.status(200).json(contacts);
+app.get('/contacts', async (req, res, next) => {
+    try {
+        const contacts = await Contact.find();
+        res.status(200).json(contacts); 
+    } catch(err) {
+        next(err);
+    }
 });
 
 app.use('/', web );
@@ -33,6 +36,16 @@ app.post('*', function(req, res) {
     res.status(404).json({ status: 404, message: 'Not found'});
 });
 
+const HTTP_SERVER_ERROR = 500;
+app.use(function(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  console.error(err);
+  const status = err.status || HTTP_SERVER_ERROR;
+  return res.status(status).json({ status: status, message: 'Internal server error' });
+});
+
 app.listen(PORT, () => {
-    console.log(`ceim-commercial service running on port ${PORT}`);
+    console.log(`ceim-commercial service running on http://localhost:${PORT}`);
 });
